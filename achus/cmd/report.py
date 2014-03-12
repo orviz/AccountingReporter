325ddccb2dc36c6eb76d47c391696304f243b9aa
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2014-03-12 15:21:22 +0100

    Added setup.py and initial CLI to generate reports

---
+++ achus/cmd/report.py
@@ -0,0 +1,19 @@
+import sys
+
+from oslo.config import cfg
+
+import achus.reporter
+import achus.config
+
+CONF = cfg.CONF
+
+
+def main():
+    achus.config.parse_args(sys.argv)
+    report = achus.reporter.Report("pdf")
+    report.collect()
+    report.generate()
+
+
+if __name__ == "__main__":
+    main()
