a78650f4e459923c5479042408f16826603f616c
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2014-03-17 14:12:31 +0100

    Make all renderers have the same interfaces
    The PDFChart and Chart renderers have been refactored so that they have
    the same internal interfaces and public methods.
    
    Closes #15

--- achus/renderer/pdf.py
+++ achus/renderer/pdf.py
@@ -5,9 +5,6 @@ import cairosvg
 import PyPDF2
 from oslo.config import cfg
 
-# FIXME(aloga): this should be configurable and we should take
-# the class from the chart renderer
-from achus import exception
 import achus.renderer.chart
 
 CONF = cfg.CONF
@@ -20,57 +17,53 @@ logger = logging.getLogger(__name__)
 
 
 class PDFChart(achus.renderer.base.Renderer):
-    """ Generates PDF report containing charts."""
+    """
+    Generates PDF report containing charts.
+
+    The metrics added using the append_metric method
+    will be rendered into a PDF report containing charts.
+    """
 
     def __init__(self):
-        self.metrics = []
+        super(PDFChart, self).__init__()
+        self.chart = achus.renderer.chart.Chart()
 
     def append_metric(self, title, metric, metric_definition):
-        """Appends a metric to be graphed in the report."""
-        self.metrics.append((title, metric, metric_definition))
+        self.chart.append_metric(title, metric, metric_definition)
 
-    def render(self, filename=None):
-        """
-        Generates the PDF report by.
-
-        This method will render each of the metrics stored in the
-        self.metrics list into a graph, that will be then joined
-        into a PDF file.
-
-        If filename is set the PDF file will be written into that file.
-        If filename is not set, a string containing the file will be
-        returned.
-        """
+    def _generate_pdf(self):
         pdf_charts = []
-        for title, metric, metric_definition in self.metrics:
-            try:
-                chart = achus.renderer.chart.Chart(metric,
-                                                   title,
-                                                   metric_definition["chart"])
-            except exception.UnknownChartType:
-                logging.debug("Metric is not set to be displayed as "
-                              "a chart. Note that no other format is "
-                              "supported. Doing nothing.")
-            else:
-                svg = chart.render()
-                pdf_charts.append(cairosvg.svg2pdf(bytestring=svg))
+        for chart in self.chart.render():
+            pdf_charts.append(cairosvg.svg2pdf(bytestring=chart))
 
         output = PyPDF2.PdfFileWriter()
         for pdf in pdf_charts:
             input1 = PyPDF2.PdfFileReader(StringIO.StringIO(pdf))
             output.addPage(input1.getPage(0))
 
-        if filename:
-            output_stream = file(filename, "wb")
-            output.write(output_stream)
-            output_stream.close()
-            logger.debug("Result PDF created under '%s'" % filename)
-            return None
-        else:
-            output_stream = StringIO.StringIO()
-            output.write(output_stream)
-            return output_stream.read()
+        return output
+
+    def render(self):
+        """
+        Generates the PDF report.
+
+        This method will render each of the metrics that have been added
+        into charts, that will be then joined into a PDF file.
+
+        If filename is set the PDF file will be written into that file.
+        If filename is not set, a string containing the file contents will
+        be returned.
+        """
+
+        output = self._generate_pdf()
+        output_stream = StringIO.StringIO()
+        output.write(output_stream)
+        yield output_stream.read()
 
     def render_to_file(self, filename=CONF.renderer.output_file):
         """Write the PDF report into filename"""
-        return self.render(filename=filename)
+        output = self._generate_pdf()
+        output_stream = file(filename, "wb")
+        output.write(output_stream)
+        output_stream.close()
+        logger.debug("Result PDF created under '%s'" % filename)
